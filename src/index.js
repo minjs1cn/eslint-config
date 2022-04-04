8c52ca8c05312ae4349c403e14393ed0adc347dd
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-04-04 18:42:54 +0800

    release

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const basic = require("./basic");
-const vue = require("./vue");
-const react = require("./react");
-const typescript = require("./typescript");
-
 module.exports = {
-  basic,
-  vue,
-  react,
-  typescript,
+  extends: ["./basic"],
 };
