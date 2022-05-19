d517e80ab4c748e1e831024285db103c375cacbe
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-05-19 09:46:04 +0800

    fix: index

--- react.js
+++ react.js
@@ -10,4 +10,8 @@ module.exports = {
       version: 'detect',
     },
   },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': [ 'error', 2 ],
+  },
 };
--- vue.js
+++ vue.js
@@ -20,4 +20,8 @@ module.exports = {
   env: {
     'vue/setup-compiler-macros': true,
   },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': [ 'error', 2 ],
+  },
 };
