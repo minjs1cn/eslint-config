f7105688319b8b41d09e9f8f50ffa529bf033838
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-05-19 08:14:17 +0800

    fix: index 冲突

--- typescript.js
+++ typescript.js
@@ -8,4 +8,8 @@ module.exports = {
     },
   },
   extends: [ './basic', 'plugin:@typescript-eslint/recommended' ],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': [ 'error', 2 ],
+  },
 };
